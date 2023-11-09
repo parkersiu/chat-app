eb2dae2c3fa157dd7d759fab55b5010885034c38
Parker Siu <parker.siu@gmail.com>
2023-11-08 17:01:24 -0800

    Adjusted profile dropdown for pro users.

--- components/UserButton.tsx
+++ components/UserButton.tsx
@@ -12,9 +12,13 @@ import { Session } from "next-auth";
 import { Button } from "./ui/button";
 import UserAvatar from "./UserAvatar";
 import { signIn, signOut } from "next-auth/react";
+import { useSubscriptionStore } from "@/store/store";
+import LoadingSpinner from "./LoadingSpinner";
+import { StarIcon } from "lucide-react";
 
 function UserButton({ session }: { session: Session | null }) {
-  // Subscription listener
+  const subscription = useSubscriptionStore((state) => state.subscription);
+
   if (!session)
     return (
       <Button variant={"outline"} onClick={() => signIn()}>
@@ -30,6 +34,29 @@ function UserButton({ session }: { session: Session | null }) {
         <DropdownMenuContent>
           <DropdownMenuLabel>{session.user?.name}</DropdownMenuLabel>
           <DropdownMenuSeparator />
+
+          {subscription === undefined && (
+            <DropdownMenuItem>
+              <LoadingSpinner />
+            </DropdownMenuItem>
+          )}
+
+          {subscription?.role === "pro" && (
+            <>
+              <DropdownMenuLabel className="text-xs flex items-center justify-center space-x-1 text-[#E935C1] animate-pulse">
+                <StarIcon fill="#E935C1" />
+                <p>PRO</p>
+              </DropdownMenuLabel>
+
+              <DropdownMenuSeparator />
+
+              <DropdownMenuItem>
+                Manage
+                {/* <ManageAccountButton /> */}
+              </DropdownMenuItem>
+            </>
+          )}
+
           <DropdownMenuItem onClick={() => signOut()}>
             Sign Out
           </DropdownMenuItem>
