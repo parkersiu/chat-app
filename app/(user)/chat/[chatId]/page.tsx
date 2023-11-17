69d596b407a82a7bf0d1ebe2ebf919d496daebe8
Parker Siu <parker.siu@gmail.com>
2023-11-17 14:38:04 -0800

    Added chat members badge and useAdminId hook.

--- app/(user)/chat/[chatId]/page.tsx
+++ app/(user)/chat/[chatId]/page.tsx
@@ -1,5 +1,6 @@
 import { authOptions } from "@/auth";
 import ChatInput from "@/components/ChatInput";
+import ChatMembersBadges from "@/components/ChatMembersBadges";
 import ChatMessages from "@/components/ChatMessages";
 import { sortedMessagesRef } from "@/lib/converters/Message";
 import { getDocs } from "firebase/firestore";
@@ -21,7 +22,7 @@ async function ChatPage({ params: { chatId } }: Props) {
     <>
       {/* Admin Controls */}
       {/* Chat Members Badge */}
-      {/* Chat Messages */}
+      <ChatMembersBadges chatId={chatId} />
       <div className="flex-1">
         <ChatMessages
           chatId={chatId}
