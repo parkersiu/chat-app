8b7dfb8269404950e21ca042b868e7a9526a999f
Parker Siu <parker.siu@gmail.com>
2023-11-16 19:19:07 -0800

    Added chat input to chats page.

--- app/(user)/chat/[chatId]/page.tsx
+++ app/(user)/chat/[chatId]/page.tsx
@@ -1,10 +1,22 @@
-function ChatPage() {
+import { authOptions } from "@/auth";
+import ChatInput from "@/components/ChatInput";
+import { getServerSession } from "next-auth";
+
+type Props = {
+  params: {
+    chatId: string;
+  };
+};
+
+async function ChatPage({ params: { chatId } }: Props) {
+  const session = await getServerSession(authOptions);
+
   return (
     <>
       {/* Admin Controls */}
       {/* Chat Members Badge */}
       {/* Chat Messages */}
-      {/* Chat Input */}
+      <ChatInput chatId={chatId} />
     </>
   );
 }
